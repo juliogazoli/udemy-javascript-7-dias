9902c3df734d572fa89c1acd2a6748fc5c89dc76
Julio Gazoli <julio.gazoli@sorocaba.sp.gov.br>
2024-04-01 12:46:49 -0300

    constantes

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-console.log('Javascript Funcionando');
-
-let firstName = 'Julio';
-let lastName = 'Gazoli';
-console.log(firstName);
-console.log(lastName);
+const price = 30;
+// price = 20;
+console.log(price);
