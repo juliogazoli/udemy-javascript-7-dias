4940906542342cdfe97a595d47df3b0699b8dd1e
Julio Gazoli <julio.gazoli@sorocaba.sp.gov.br>
2024-04-02 11:03:04 -0300

    objetos

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 let itemName = 'Pen'
 let itemPrice = 3
 let itemAvailable = true
-let itemColor
-itemPrice = 'dois'
+let itemColor = 'Red'
 
-console.log(typeof itemPrice)
+let pen = {
+    itemName: 'Pen',
+    itemPrice: 3,
+    itemAvailable: true,
+    itemColor: 'Red'
+}
+
+pen.itemColor = 'Blue'
+
+console.log(pen)
+console.log(pen.itemName)
