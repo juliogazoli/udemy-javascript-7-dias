1fb439785163e7d29401e639aee4c602433585f9
Julio Gazoli <julio_gazoli@hotmail.com>
2024-04-04 17:24:31 -0300

    if e else

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-// let num1 = 3 + 4 * 2
-let num1 = (3 + 4) * 2
-console.log(num1)
+let driverSpeed = 110
+
+if (driverSpeed > 110) {
+    console.log('Driving too fast')
+}
+
+else if (driverSpeed > 40 && driverSpeed <= 110) {
+    console.log('OK')
+}
+
+else {
+    console.log('Driving too slow')
+}
