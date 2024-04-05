d87b4faa04596655c5928a44f97566c50a3ca016
Julio Gazoli <julio_gazoli@hotmail.com>
2024-04-05 15:45:34 -0300

    switch e case

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-let driverSpeed = 110
+let airport = 'ABC'
 
-if (driverSpeed > 110) {
-    console.log('Driving too fast')
-}
-
-else if (driverSpeed > 40 && driverSpeed <= 110) {
-    console.log('OK')
-}
-
-else {
-    console.log('Driving too slow')
+switch (airport) {
+    case 'MCO':
+        console.log('Orlando')
+        break
+    case 'JFK':
+        console.log('John F. Kennedy')
+        break
+    case 'SEA':
+        console.log('Seattle')
+        break
+    default:
+        console.log('Unknown')
 }
