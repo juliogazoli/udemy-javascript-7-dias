3bab0f25b7918daa049f63fc71e53c0225380eb1
Julio Gazoli <julio_gazoli@hotmail.com>
2024-04-11 15:59:18 -0300

    criando uma factory

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-const book = {
-    bookTitle: 'Atomic Habits',
-    bookAuthor: 'James Clear',
-    bookPages: 306,
-    bookChaptters: {
-        chap1: 'Fundamentals',
-        chap2: '1st law',
-    },
-    printBook: function () {
-        console.log('printing...')
+function createBook(title, author, pages) {
+    const book = {
+        bookTitle: title,
+        bookAuthor: author,
+        bookPages: pages,
+
+        printBook: function () {
+            console.log('printing...')
+        }
     }
+    return book
 }
 
-book.printBook()
+const book1 = createBook('Atomic', 'James', 306)
+const book2 = createBook('Think', 'Napolean', 450)
+
+console.log(book1)
+console.log(book2)
