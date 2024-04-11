0f2d283419e85c3ef7cea2897100802c22c492d2
Julio Gazoli <julio_gazoli@hotmail.com>
2024-04-11 16:10:56 -0300

    sobre o garbage collector

--- index.js
+++ index.js
@@ -1,9 +1,2 @@
-function CreateBook(title, author, pages) {
-    this.bookTitle = title;
-    this.bookAuthor = author;
-    this.bookPages = pages
-}
-
-const book1 = new CreateBook('Atomic', 'James', 306)
-
-console.log(book1)
+let firstName = 'Julio'
+console.log(firstName)
